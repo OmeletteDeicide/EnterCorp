13121d16c671ea6176d38257a7179d76e379f307
flocorbab <florian.corridoni@gmail.com>
2023-04-07 17:20:24 +0200

    petit test de commit

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+//modification
 class IndexController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
