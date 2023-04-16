860d383aa2216e805ac4accb7424ad4e3d382af9
flocorbab <51161916+flocorbab@users.noreply.github.com>
2023-04-16 15:44:13 +0200

    new

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+<<<<<<< Updated upstream
 //modification
+=======
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+>>>>>>> Stashed changes
 class IndexController extends AbstractController
 {
-    #[Route('/index', name: 'app_index')]
-    public function index(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $categories = $entityManager->getRepository(Category::class)->findAll();
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'controller_name' => 'IndexController', 'categories' => $categories
         ]);
     }
 }
